b25d20594a898cc37e6b91d8a89267f59444d716
Kirill Usichenko <kirillusichenko@gmail.com>
2022-06-02 15:47:42 +0300

    fix: change add button

--- src/pages/album/Album.tsx
+++ src/pages/album/Album.tsx
@@ -8,7 +8,7 @@ import {
     Img,
     Photos,
     PhotoWrapper,
-    LoaderWrapper, MainWrapper
+    LoaderWrapper, MainWrapper, Location, AddButton, NavLeft
 } from "./AlbumStyles";
 import {Wrapper} from "../../components/Container/Container";
 import {Link, useNavigate, useParams} from "react-router-dom";
@@ -60,43 +60,55 @@ const Album = () => {
         newFile.type = file.type.split('/').slice(1, 2).join('/')
         newFile.webkitRelativePath = file.webkitRelativePath
         setPhoto(file);
-    }
-    const sendImage = (e: any) => {
-        dispatch(addPhoto(photo.name, userId, album.id, photo.type, photo, photos))
+        dispatch(addPhoto(file.name, userId, album.id, file.type, file, photos))
         setPhoto(null)
-
     }
+    // const sendImage = (e: any) => {
+    //     dispatch(addPhoto(photo.name, userId, album.id, photo.type, photo, photos))
+    //     setPhoto(null)
+    //
+    // }
     return (
         <Wrapper>
             <Inner>
                 <Nav>
-                    <Back>
-                        <Link to={"/albums"}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                                <g fill="none" fillRule="evenodd">
-                                    <g>
+                    <NavLeft>
+                        <Back>
+                            <Link to={"/albums"}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                                    <g fill="none" fillRule="evenodd">
                                         <g>
                                             <g>
                                                 <g>
                                                     <g>
-                                                        <path d="M0 0L24 0 24 24 0 24z"
-                                                              transform="translate(-24 -120) translate(0 96) translate(24 24)"/>
-                                                        <path fill="#21272E"
-                                                              d="M19 11H7.83l4.88-4.88c.39-.39.39-1.03 0-1.42-.39-.39-1.02-.39-1.41 0l-6.59 6.59c-.39.39-.39 1.02 0 1.41l6.59 6.59c.39.39 1.02.39 1.41 0 .39-.39.39-1.02 0-1.41L7.83 13H19c.55 0 1-.45 1-1s-.45-1-1-1z"
-                                                              transform="translate(-24 -120) translate(0 96) translate(24 24)"/>
+                                                        <g>
+                                                            <path d="M0 0L24 0 24 24 0 24z"
+                                                                  transform="translate(-24 -120) translate(0 96) translate(24 24)"/>
+                                                            <path fill="#21272E"
+                                                                  d="M19 11H7.83l4.88-4.88c.39-.39.39-1.03 0-1.42-.39-.39-1.02-.39-1.41 0l-6.59 6.59c-.39.39-.39 1.02 0 1.41l6.59 6.59c.39.39 1.02.39 1.41 0 .39-.39.39-1.02 0-1.41L7.83 13H19c.55 0 1-.45 1-1s-.45-1-1-1z"
+                                                                  transform="translate(-24 -120) translate(0 96) translate(24 24)"/>
+                                                        </g>
                                                     </g>
                                                 </g>
                                             </g>
                                         </g>
                                     </g>
-                                </g>
-                            </svg>
-                        </Link>
-                    </Back>
-                    <AlbumHeader>
-                        <Name>{album.name}</Name>
-                        <Name>{album.location}</Name>
-                    </AlbumHeader>
+                                </svg>
+                            </Link>
+                        </Back>
+                        <AlbumHeader>
+                            <Name>{album.name}</Name>
+                            <Location>{album.location}</Location>
+                        </AlbumHeader>
+                    </NavLeft>
+                    <AddButton onClick={onAddClick}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                             stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
+                             className="feather feather-plus">
+                            <line x1="12" y1="5" x2="12" y2="19"/>
+                            <line x1="5" y1="12" x2="19" y2="12"/>
+                        </svg>
+                    </AddButton>
                 </Nav>
                 {isLoading ? <LoaderWrapper><Loader/></LoaderWrapper> : (
                     <MainWrapper style={{width: '100%'}}>
@@ -113,8 +125,8 @@ const Album = () => {
                                    onChange={onUploadChange}
                                    style={{display: "none"}}
                             />
-                            <Button onClick={onAddClick}>Add photo</Button>
-                            <Button onClick={sendImage}>Accept</Button>
+                            {/*<Button onClick={onAddClick}>Add photo</Button>*/}
+                            {/*<Button onClick={sendImage}>Accept</Button>*/}
                         </div>
                     </MainWrapper>
                 )
